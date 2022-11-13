0948a1d4c3bbe4f6db7db6c3210f91904e51dd8f
pattymicaelazurita <patty.zurita@yahoo.com>
2022-11-13 03:35:23 -0500

    Mejoras de texto en CRUD de Administrador

--- templates/Administrador/add.php
+++ templates/Administrador/add.php
@@ -8,21 +8,21 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Administrador'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Lista de Administradores'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="administrador form content">
             <?= $this->Form->create($administrador) ?>
             <fieldset>
-                <legend><?= __('Add Administrador') ?></legend>
+                <legend><?= __('Agregar Administrador') ?></legend>
                 <?php
                     echo $this->Form->control('nombre');
                     echo $this->Form->control('contrasenia');
                     echo $this->Form->control('correo');
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Agregar')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
